8908d4b8fcb0f58fcb636fcfb4454e374574922c
sjb <2224546997@qq.com>
2019-11-08 19:30:31 +0800

    aa

---
+++ src/test/java/com/sjb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
